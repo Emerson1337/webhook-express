3c052c5c920a9a701c55bbb60a145e758c9e0cec
Emerson <emersonlucenaoficial@gmail.com>
2023-02-06 11:44:30 -0300

    chore: create initial configuration

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { Router, Request, Response } from "express";
+import fs from "fs";
+
+const app = express();
+const route = Router();
+
+app.use(express.json());
+
+route.get("/", (req: Request, res: Response) => {
+  res.json({ message: "hello world with Typescript" });
+});
+
+route.post("/send-data", (req: Request, res: Response) => {
+  const date = new Date().toJSON();
+  fs.writeFile(`data/${date}.json`, req.body, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  res.json({ message: "saved!" });
+});
+
+app.use(route);
+
+app.listen(3332, () => console.log("Server has been started! 🚀"));
