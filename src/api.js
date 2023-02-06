11402286d3f0204268577487bb582eb7be3210b4
Emerson <emersonlucenaoficial@gmail.com>
2023-02-06 13:24:40 -0300

    feat: save data on file

---
+++ src/api.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!",
+  });
+});
+
+router.post("/send-data", (req, res) => {
+  const date = Date.now();
+
+  fs.writeFile(`${date}.json`, JSON.stringify(req.body), function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  res.json({ message: "saved!" });
+});
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
