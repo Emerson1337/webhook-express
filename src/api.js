0c93ccb47439352808b97e4a8f46e8f28e49f9ff
Emerson <emersonlucenaoficial@gmail.com>
2023-02-06 13:41:36 -0300

    feat: list all data

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const serverless = require("serverless-http");
 
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const router = express.Router();
@@ -24,6 +25,25 @@ router.post("/send-data", (req, res) => {
   });
 
   res.json({ message: "saved!" });
+});
+
+router.get("/get-data", (req, res) => {
+  const jsonsInDir = fs
+    .readdirSync("./")
+    .filter(
+      (file) =>
+        path.extname(file) === ".json" &&
+        !path.basename(file).includes("package")
+    );
+
+  const allContents = [];
+
+  jsonsInDir.forEach((file) => {
+    const fileData = fs.readFileSync(path.join("./", file)).toString();
+    allContents.push(JSON.parse(fileData.toString()));
+  });
+
+  res.json(allContents);
 });
 
 app.use(`/.netlify/functions/api`, router);
